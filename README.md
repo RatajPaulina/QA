8cf44418626dac91608f83ebdb661b5c112fb13c
Paulina <p.rataj@clearcode.cc>
2021-12-08 14:23:04 +0100

    3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Zadanie 
+# Zadanie 1
 
 1. Stwórz własną paczkę NPMową
 2. Dorzuć do package.json skrypt, który umożliwia uruchomienie kodu z pliku *(npm run start)*
@@ -6,3 +6,9 @@
 4. Importuj bibliotekę we własnym kodzie
 5. Przy użyciu biblioteki wykonaj GET request do goweatherapi aby pobrać informacje na temat pogody we Wrocławiu (https://goweather.herokuapp.com/weather/Wroclaw)
 6. Sprawdź czy w ciągu najbliższych 3 dni wystąpi chociaż raz temperatura przynajmniej +15 stopni i zwróć stosowną informację przy każdym uruchomieniu programu (npm run start)
+
+# Zadanie 2
+
+Dowolność - przedstaw wybrany wycinek rzeczywistości za pomocą klas i obiektów :)
+Skorzystaj z kopiowania obiektów, hermetyzacji, pól i metod statycznych, dziedziczenia, getterów/setterów i nadpisywania metod
+Wstrzyknij zależność w konstruktorze (dependency injection)
--- weather.js
+++ weather.js
@@ -3,33 +3,35 @@ import fetch from 'node-fetch';
 function temperatuea(temp, body){
     let cold= 0;
     let warm = 0;
-    let myPromise = new Promise (function(myReslove, myReject){
+    let myPromise = new Promise (function(myReslove){
 
         for (var i=0; i < 3; i++) {
-        var tempDay = parseInt(body.forecast[i].temperature.replace(/[^\d.-]/g,''))
-        console.log('Dnia '+ i + ' temperatura wyniesie ' + tempDay + ' °C')
-            if(tempDay <= temp){
-                console.log(tempDay+'<='+temp)
-                warm ++
-            } else{ 
-                cold++
+            const tempDay = parseInt(body.forecast[i].temperature.replace(/[^\d.-]/g,''))
+            console.log('Dnia '+ i + ' temperatura wyniesie ' + tempDay + ' °C')
+                if(tempDay>=temp){
+                    myReslove(' Z tego wynika, ze bedzie chociaz raz ciepło! ')
+                    warm ++
+                } else{ 
+                    //console.log(' Bedzie zimno :( ')
+                    cold ++
+                }
             }
-        }
-    })
-
-    myPromise.then(
-        console.log('Bedzie ciepło '+ warm + ' dni'),
-        console.log('Bedzie zimno '+ cold + ' dni')
-    )
+        })
     
+        myPromise.then(
+            function(myReslove) {console.log(myReslove);},
+            console.log('Bedzie ciepło '+ warm + ' dni'),
+            console.log('Bedzie zimno '+ cold + ' dni' + '\n')
+        )
     
 }
 
 void(async () =>{
-    const response = await fetch('https://goweather.herokuapp.com/weather/Wroclaw');
+    //const response = await fetch('https://goweather.herokuapp.com/weather/Wroclaw');
+    const response = await fetch('https://goweather.herokuapp.com/weather/Curitiba');
     const body = await response.json();
-    let tempString ='-7 °C'
-    console.log('Temperatura od której oceniamy czy jest ciepło czy zimno wynosi ' + tempString)
+    let tempString ='16 °C'
+    console.log('Temperatura od której oceniamy czy jest ciepło czy zimno wynosi ' + tempString +'\n')
     var temp =parseInt(tempString.replace(/[^\d.-]/g,''))
     
     await temperatuea(temp,body)
